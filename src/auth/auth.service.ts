1f9dbbd35c10f2bf799973d5974c63b24af94df2
Jhosef <jmperezperez25@gmail.com>
2024-11-21 08:50:04 -0500

    encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,43 @@
-import { Body, Injectable } from '@nestjs/common';
+import { BadRequestException, Body, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserhDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/auth.entity';
+import * as bcryptjs from 'bcryptjs'
 
 @Injectable()
 export class AuthService {
-  create( createAuthDto:CreateUserhDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+     @InjectModel( User.name ) 
+    private userModel: Model<User>,
+  
+  ){}
+
+  async create( CreateUserhDto:CreateUserhDto): Promise<User> {
+    
+    try
+    {
+      const { password , ...userData } = CreateUserhDto
+      const newUser = new this.userModel({
+        password :bcryptjs.hashSync( password , 10 ),
+
+      });
+
+
+
+      return await newUser.save();
+  }
+  catch (error) {
+      
+      if( error.code === 11000 )
+        throw new BadRequestException( `${ CreateUserhDto.email } already exist ` )
+      
+      throw new InternalServerErrorException( 'Big crash')
+      
+    }
+    
   }
 
   findAll() {
