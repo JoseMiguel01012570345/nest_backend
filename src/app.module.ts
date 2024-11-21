374d2b2dfa5fcdeaf8c52a3382ff081c1ff135cf
Jhosef <jmperezperez25@gmail.com>
2024-11-21 08:00:03 -0500

    connecion added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot() ,
+    MongooseModule.forRoot(process.env.MONGO_URL) ,
+    AuthModule , 
+  ],
+  controllers: [],
+  providers: [],
   exports: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
---
+++ src/auth/entities/auth.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+
+@Schema()
+export class User {
+
+    @Prop({unique: true , require:true})
+    email:string
+    
+    @Prop({require:true})
+    name: string
+    
+    @Prop({minlength: 6 , require: true})
+    password:string
+    
+    @Prop({default: true  })
+    isActive: boolean
+    
+    @Prop({type: [ String] , default: ['user']  })
+    roles:string[]    
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
